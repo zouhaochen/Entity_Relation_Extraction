8d44b2e52da215a770c8309a17b2c98eff460ab7
zouhaochen <zouhaochenluke@gmail.com>
2023-11-14 23:38:30 +0800

    process create

--- process.py
+++ process.py
@@ -7,11 +7,17 @@ def generate_rel():
     with open(SCHEMA_PATH) as f:
         rel_list = []
         for line in f.readlines():
+
+            # json.load加载文本转化字典类型
             info = json.loads(line)
+
+            # 取字典内关系predicate值
+            # rel_list: id to relation
             rel_list.append(info['predicate'])
         rel_dict = {v: k for k, v in enumerate(rel_list)}
         df = pd.DataFrame(rel_dict.items())
         df.to_csv(REL_PATH, header=None, index=None)
+
 
 if __name__ == '__main__':
     generate_rel()
